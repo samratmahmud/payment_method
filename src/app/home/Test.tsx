3b7bfa545243c057c764d28500342568f9afb295
samratmahmud <samratmahmud1999@gmail.com>
2023-11-05 00:15:11 +0600

    pay

---
+++ src/app/home/Test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Test() {
+  return (
+    <div className="pt-5">
+      <div className="my-5 absolute">
+        <img src="/picture/Vector.png" alt="" />
+      </div>
+      <div className="flex items-center gap-5 relative px-10">
+        <div>
+          <img src="/picture/AdobeStock_91101792_Preview 1.png" alt="" />
+        </div>
+        <div>
+          <div className="text-xl">Our goals</div>
+          <div>
+            We aim to reduce the diameter and increase the hollow rate of hollow
+            monofilament by 50%, while maintaining the desired performance
+            targets.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import Navbar from "@/components/global/Navbar";
 import Header from "./home/Header";
 import Card from "./home/Card";
+import Test from "./home/Test";
 
 export default function Home() {
   return (
     <div className="container pb-6">
-      <div className="lg:border-r border-b lg:fixed top-0 bottom-0 bg-white border-gray-300 overflow-y-scroll">
+      <div className="lg:border-r border-b lg:fixed top-0 bottom-0 bg-white border-gray-300">
         <Navbar />
       </div>
       <div className="flex-grow lg:ml-[290px]">
         <Header />
         <Card />
       </div>
+      {/* <Test /> */}
     </div>
   );
 }
